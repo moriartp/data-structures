9e9c06067f81cb94c0b78b93cf594d572160c02f
moriartp <moriartp@newschool.edu>
2016-09-08 14:40:19 +0000

    basic scrape of target dir

--- week2/aa-scraper.js
+++ week2/aa-scraper.js
@@ -1,15 +1,23 @@
 var fs = require('fs');
 var cheerio = require('cheerio');
 
-var content = fs.readFileSync('/home/ubuntu/workspace/data/week2/UESAA.txt');
+var content = fs.readFileSync('/home/ubuntu/workspace/week2/UESAA.txt');
 
 var $ = cheerio.load(content);
 
+// // // STARTER CODE
+// // Print to console: all reading assignments
+// $('h4').each(function(i, elem) {
+//     if ($(elem).text() == "Read") {
+//         $(elem).next().find('li').each(function(i, elem) {
+//             console.log($(elem).text());
+//         });
+//     }
+// })
+
 // Print to console: all reading assignments
-$('h4').each(function(i, elem) {
-    if ($(elem).text() == "Read") {
-        $(elem).next().find('li').each(function(i, elem) {
+$('td').each(function(i, elem) {
+        $(elem).find('b').each(function(i, elem) {
             console.log($(elem).text());
         });
-    }
 })
