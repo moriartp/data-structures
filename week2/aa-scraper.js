9543bd5571ee470c7a1f502d41de15488e71dfb4
moriartp <moriartp@newschool.edu>
2016-09-14 19:44:05 +0000

    solution

--- week2/aa-scraper.js
+++ week2/aa-scraper.js
@@ -1,39 +1,56 @@
-var fs = require('fs');
-var cheerio = require('cheerio');
-var content = fs.readFileSync('/home/ubuntu/workspace/week2/UESAA.html');
-var $ = cheerio.load(content);
+// var fs = require('fs');
+// var cheerio = require('cheerio');
+// var content = fs.readFileSync('/home/ubuntu/workspace/week2/UESAA.html');
+// var $ = cheerio.load(content);
 
-// // // STARTER CODE
-// // Print to console: all reading assignments
-// $('h4').each(function(i, elem) {
-//     if ($(elem).text() == "Read") {
-//         $(elem).next().find('li').each(function(i, elem) {
-//             console.log($(elem).text());
-//         });
-//     }
-// })
+// // // // STARTER CODE
+// // // Print to console: all reading assignments
+// // $('h4').each(function(i, elem) {
+// //     if ($(elem).text() == "Read") {
+// //         $(elem).next().find('li').each(function(i, elem) {
+// //             console.log($(elem).text());
+// //         });
+// //     }
+// // })
+
+// // // // V1 SCRAPED Bs
+// // // Print to console: all reading assignments
+// // $('td').each(function(i, elem) {
+// //         $(elem).find('b').each(function(i, elem) {
+// //             console.log($(elem).text());
+// //         });
+// // })
+
+// // // V1 SCRAPED Bs
+// // // Print to console: all reading assignments
+// // $('tbody').each(function(i, elem) {
+// //         $(elem).find('tr').each(function(i, elem) {
+// //             console.log($(elem).text());
+// //         });
+// // })
 
 // // // V1 SCRAPED Bs
 // // Print to console: all reading assignments
-// $('td').each(function(i, elem) {
-//         $(elem).find('b').each(function(i, elem) {
-//             console.log($(elem).text());
-//         });
-// })
-
-// // V1 SCRAPED Bs
-// // Print to console: all reading assignments
 // $('tbody').each(function(i, elem) {
-//         $(elem).find('tr').each(function(i, elem) {
+//         $(elem).find('td').each(function(i, elem) {
 //             console.log($(elem).text());
 //         });
-// })
+// });
 
-// // V1 SCRAPED Bs
-// Print to console: all reading assignments
-$('tbody').each(function(i, elem) {
-        $(elem).find('td').each(function(i, elem) {
-            console.log($(elem).text());
-        });
-});
+/////////////////////////////////////////////////////////////////////////
+
+var fs = require('fs');
+var cheerio = require('cheerio'); // npm install cheerio
+
+var content = fs.readFileSync('/home/ubuntu/workspace/week2/UESAA.html');
+var meetings = [];
+
+var $ = cheerio.load(content);
+
+$('tbody').find('tr').each(function(i, elem){
+        meetings.push($(elem).find('td').eq(0).html().split('<br>')[2].trim());
+    });
+
+console.log(meetings.length); // print number of meetings in meetings array
+fs.writeFileSync('/home/ubuntu/workspace/week2/meetingsArray.txt', JSON.stringify(meetings));
 
