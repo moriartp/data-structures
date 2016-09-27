b4f04e684922bb8f68015ac9aae369809151fff9
moriartp <moriartp@newschool.edu>
2016-09-27 23:18:08 +0000

    request url

--- week4/week4.js
+++ week4/week4.js
@@ -11,13 +11,12 @@
 var request = require('request');
 var fs = require('fs');
 
-request('aa-latLong.json', function(error, response, body) {
+request('https://raw.githubusercontent.com/moriartp/data-structures/master/week3/aa-latLong.txt', function(error, response, body) {
     var meetingData = JSON.parse(body);
 
     // Connection URL
-    var url = 'mongodb://' + process.env.IP + ':27017/aa';
-    // var database = process.env.IP + 'week4/aa';
-    // var dataFile = fs.readFile('https://raw.githubusercontent.com/moriartp/data-structures/master/week4/aa-latLong.json');
+    var url = 'mongodb://localhost/:27017/week4';
+
 
     // Retrieve
     var MongoClient = require('mongodb').MongoClient; // npm install mongodb
