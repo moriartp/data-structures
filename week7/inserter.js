552dfadffe07ea90d96bb7b4bd0357c593515ae9
moriartp <moriartp@newschool.edu>
2016-11-22 16:05:22 +0000

    valid inserter.js file

---
+++ week7/inserter.js
@@ -0,0 +1,39 @@
+// IN THE MONGO SHELL: 
+//   CREATE DATABASE citibike AND SWITCH TO IT WITH: 
+//      use citibike
+//   CREATE COLLECTION stations WITH: 
+//      db.createCollection('stations')
+//   QUERY THE ENTIRE stations COLLECTION WITH:
+//      db.stations.find()
+//   COUNT THE NUMBER OF DOCUMENTS IN THE stations COLLECTION WITH:
+//      db.stations.find().count()
+
+var request = require('request');
+var fs = require('fs');
+
+request('https://raw.githubusercontent.com/moriartp/data-structures/master/week7/output/_10output.txt', function(error, response, body) {
+    var meetingData = JSON.parse(body);
+
+    // Connection URL
+    var url = 'mongodb://' + process.env.IP + ':27017/aa';
+
+
+    // Retrieve
+    var MongoClient = require('mongodb').MongoClient; // npm install mongodb
+
+    MongoClient.connect(url, function(err, db) {
+        if (err) {return console.dir(err);}
+
+        var collection = db.collection('aa_meetings');
+
+        // THIS IS WHERE THE DOCUMENT(S) IS/ARE INSERTED TO MONGO:
+        for (var i=0; i < meetingData.length; i++) {
+            collection.insert(meetingData[i]);
+            }
+        db.close();
+
+    }); //MongoClient.connect
+
+}); //request
+
+// console.log(process.env.IP);
